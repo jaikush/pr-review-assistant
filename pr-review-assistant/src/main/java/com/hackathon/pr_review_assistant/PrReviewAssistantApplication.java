401e15f1ccda298490cbafb735c097fb7dc59371
JAYAWANTH M V <Jayawanth>
2025-08-01 18:05:40 +0530

    Initial Spring Boot setup

---
+++ pr-review-assistant/src/main/java/com/hackathon/pr_review_assistant/PrReviewAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.pr_review_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrReviewAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrReviewAssistantApplication.class, args);
+	}
+
+}
---
+++ pr-review-assistant/src/test/java/com/hackathon/pr_review_assistant/PrReviewAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.pr_review_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrReviewAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
